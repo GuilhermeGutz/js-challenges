5efe10898b36004db08de0b8f9ab1041314efdf4
Guilherme <guilhermegutz@hotmail.com>
2022-02-06 23:03:55 -0300

    Add truthy-falsy-compare

---
+++ truthy-falsy-compare.js
@@ -0,0 +1,15 @@
+let wordCount = 0; //Alterar qualquer numero diferente de 0 retorna TRUE
+ 
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+ 
+let favoritePhrase = 'Hellow World'; //string vazia, ou seja ''
+ 
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
